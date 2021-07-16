30349ad123b5e741375ce21badcd8f602bde8021
Anthony <sean.a.protho1@gmail.com>
2021-07-15 18:16:44 -0700

    beeep

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	"time"
+	"github.com/gen2brain/beeep"
 )
 
 func main() {
 	for (true)  {
-		time.Sleep(1 * time.Minute)
-		fmt.Print("hello world")
+		time.Sleep(20 * time.Second) // time.Minute)
+		title := "20-20-20"
+		message := "Take A break! Look at something 20ft away for at least 20 seconds"
+		icon := "./eyes.png"
+		err := beeep.Alert(title, message, icon)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Print("Debug Message at: ", time.Now())
+		time.Sleep(20 * time.Second)
 	}
 }
